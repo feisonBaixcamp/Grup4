8f410c1a03523e7f5db58a7cfdcffc01963c3352
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-05 20:10:32 +0100

    Update db.php

--- VulnLab-main/app/lab/insecure-deserialization/full-privileges/db.php
+++ VulnLab-main/app/lab/insecure-deserialization/full-privileges/db.php
@@ -1,33 +1,27 @@
 <?php
 include("permission.php");
-class db {
 
-    
+class db {
     private $userlist;
 
     function __construct() {
-
         $this->userlist = array(
             0 => array(
-                'username' => md5('administrator'),
-                'password' => md5('a2T%tq+<=VuLJh8,}fBwU@Qttn+YR{rq'),
+                'username' => 'administrator',
+                'password' => password_hash('a2T%tq+<=VuLJh8,}fBwU@Qttn+YR{rq', PASSWORD_BCRYPT),
                 'isAdmin' => 1,
-                'permissions' => new Permission(1,1,1)
+                'permissions' => new Permission(1, 1, 1)
             ),
             1 => array(
-                'username' => md5('test'),
-                'password' => md5('test'),
+                'username' => 'test',
+                'password' => password_hash('test', PASSWORD_BCRYPT),
                 'isAdmin' => 0,
-                'permissions' =>  new Permission(0,0,0)
+                'permissions' => new Permission(0, 0, 0)
             )
         );
+    }
 
-      }
-
-   
-
-    function getUsersList(){
-
+    function getUsersList() {
         return $this->userlist;
     }
 }
