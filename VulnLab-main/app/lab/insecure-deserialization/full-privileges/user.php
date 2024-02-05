2153f3bf65ef1f563d8568c11cabec4ceb2e77d3
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-05 20:21:01 +0100

    Update user.php

--- VulnLab-main/app/lab/insecure-deserialization/full-privileges/user.php
+++ VulnLab-main/app/lab/insecure-deserialization/full-privileges/user.php
@@ -1,26 +1,21 @@
 <?php
 
-    class User{
+class User {
 
-        public $username;
-        public $password;
-        public $isAdmin;
-        public $permissions;
-        
+    public $username;
+    public $password;
+    public $isAdmin;
+    public $permissions;
 
-        function __construct($username,$password, $isAdmin,$permissions) {
-            $this->username = $username;
-            $this->password = $password;
-            $this->isAdmin = $isAdmin;
-            $this->permissions =  $permissions;
-          }
-
-        function __toString(){
-
-            return "Username : ".$this -> username." Password : ".$this -> password. " is admin? : ".$this -> isAdmin." Permissions ".$this->permissions;
-        }
-
+    public function __construct($username, $password, $isAdmin, $permissions) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->isAdmin = $isAdmin;
+        $this->permissions = $permissions;
     }
 
+    public function __toString() {
+        return "Username: " . $this->username . " Password: " . $this->password . " Is admin? : " . $this->isAdmin . " Permissions: " . $this->permissions;
+    }
 
-?>
+}
