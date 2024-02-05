5d080483650ce72c959880b0707c4c17853b9bae
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-05 19:46:20 +0100

    Update test.php

--- VulnLab-main/app/lab/xxe/xml/test.php
+++ VulnLab-main/app/lab/xxe/xml/test.php
@@ -1,11 +1,13 @@
 <?php
-  libxml_disable_entity_loader (false);
+  // Desactivar la carga de entidades externas
+  libxml_disable_entity_loader(true);
 
   $xmlfile = file_get_contents('php://input');
   $dom = new DOMDocument();
 
+  // Cargar XML con la opción LIBXML_NOENT
   $dom->loadXML($xmlfile, LIBXML_NOENT);
 
-echo $dom->textContent;
-
+  // Imprimir el contenido seguro
+  echo htmlspecialchars($dom->textContent, ENT_QUOTES, 'UTF-8');
 ?>
