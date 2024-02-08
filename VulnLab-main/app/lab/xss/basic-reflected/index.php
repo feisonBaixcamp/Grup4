aa2880b4bf1533baa869e3102e79e2269dc5d928
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-08 19:55:59 +0100

    Update index.php

--- VulnLab-main/app/lab/xss/basic-reflected/index.php
+++ VulnLab-main/app/lab/xss/basic-reflected/index.php
@@ -19,36 +19,36 @@ $q_escaped = safe_output($q);
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-  <title><?php echo safe_output($strings['title']); ?></title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <title><?php echo safe_output($strings['title']); ?></title>
 </head>
 
 <body>
-  <div class="container d-flex justify-content-center align-items-center h-100 mx-auto">
-    <?php
+    <div class="container d-flex justify-content-center align-items-center h-100 mx-auto">
+        <?php
 
-    if (!empty($q_escaped)) {
-      echo '<div class="alert alert-danger" style="margin-top: 30vh;" role="alert" >';
-      echo safe_output($strings['text']) . ' <b>' . $q_escaped . ' </b> ';
-      echo '<a href="index.php">' . safe_output($strings['try']) . '</a>';
-      echo "</div>";
-    } else {
-      echo '<form method="GET" action="#" style="margin-top: 30vh;" class="row g-3 col-md-6 row justify-content-center mx-auto">';
-      echo '<input class="form-control" type="text" placeholder="' . safe_output($strings['search']) . '" name="q">';
-      echo '<button type="submit" class="col-md-3 btn btn-primary mb-3">' . safe_output($strings['s_button']) . '</button>';
-      echo '</form>';
-    }
+        if (!empty($q_escaped)) {
+            echo '<div class="alert alert-danger" style="margin-top: 30vh;" role="alert" >';
+            echo safe_output($strings['text']) . ' <b>' . $q_escaped . ' </b> ';
+            echo '<a href="index.php">' . safe_output($strings['try']) . '</a>';
+            echo "</div>";
+        } else {
+            echo '<form method="GET" action="#" style="margin-top: 30vh;" class="row g-3 col-md-6 row justify-content-center mx-auto">';
+            echo '<input class="form-control" type="text" placeholder="' . safe_output($strings['search']) . '" name="q">';
+            echo '<button type="submit" class="col-md-3 btn btn-primary mb-3">' . safe_output($strings['s_button']) . '</button>';
+            echo '</form>';
+        }
 
-    ?>
+        ?>
 
-  </div>
+    </div>
 
-  <script id="VLBar" title="<?= safe_output($strings['title']) ?>" category-id="1" src="/public/assets/js/vlnav.min.js"></script>
+    <script id="VLBar" title="<?php echo safe_output($strings['title']); ?>" category-id="1" src="/public/assets/js/vlnav.min.js"></script>
 
 </body>
 
