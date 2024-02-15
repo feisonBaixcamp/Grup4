620d5dfd51542ae3a244fd5359d9dbae4846e764
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-15 20:21:16 +0100

    Update index.php

--- VulnLab/app/lab/xss/basic-dom-based/index.php
+++ VulnLab/app/lab/xss/basic-dom-based/index.php
@@ -26,33 +26,27 @@ $strings = tr();
             <form action="#" method="get">
                 <div class="justify-content-center pb-3">
                     <label for="height" class="form-label"><?php echo htmlspecialchars($strings['height']); ?></label>
-                    <input type="text" name="height" id="" class="col-md-5">
+                    <input type="text" name="height" id="height" class="col-md-5">
                 </div>
                 <div class="justify-content-center">
                     <label for="base" class="form-label"><?php echo htmlspecialchars($strings['base']); ?></label>
-                    <input type="text" name="base" id="" class="col-md-5 ms-3">
+                    <input type="text" name="base" id="base" class="col-md-5 ms-3">
                 </div>
                 <button class="col-md-3 btn btn-primary mb-3 mt-4 row" style="text-align: center;"><?php echo htmlspecialchars($strings['button']); ?></button>
             </form>
         </div>
         <?php
         if (isset($_GET['base']) && isset($_GET['height'])) {
-            $height = json_encode($_GET['height']);
-            $base = json_encode($_GET['base']);
+            $base = htmlspecialchars($_GET['base']);
+            $height = htmlspecialchars($_GET['height']);
             $ans = $base * $height / 2;
-        ?>
-            <div class=" row justify-content-center text-center pt-lg-4">
-                <div class="alert alert-success col-md-5 mb-lg-5" id="answer" role="alert" style="text-align: center;">
-                    <?php echo htmlspecialchars($strings['alert']) . ' ' . $ans; ?>
-                </div>
-            </div>
-            <script>
-                var height = <?php echo $height; ?>;
-                var base = <?php echo $base; ?>;
-                var ans = <?php echo $ans; ?>;
-                document.getElementById("answer").innerHTML = "<?php echo htmlspecialchars($strings['alert']); ?> " + ans;
-            </script>
-        <?php
+            echo '<div class="row justify-content-center text-center pt-lg-4">
+                    <div class="alert alert-success col-md-5 mb-lg-5" id="answer" role="alert" style="text-align: center;"></div>
+                  </div>';
+            echo '<script>';
+            echo 'var ans = ' . json_encode($ans) . ';'; // Codificar $ans para evitar posibles ataques XSS
+            echo 'document.getElementById("answer").textContent = "' . htmlspecialchars($strings['alert']) . ' " + ans;';
+            echo '</script>';
         }
         ?>
     </div>
