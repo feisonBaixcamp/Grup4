ad4119eb0d08b59f187b0c66f41803d99784e143
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-19 20:15:14 +0100

    Update index.php

--- VulnLab/app/lab/xxe/xml/index.php
+++ VulnLab/app/lab/xxe/xml/index.php
@@ -1,18 +1,10 @@
-<?php
-    require("../../../lang/lang.php");
-    $strings = tr();
-
-    // Deshabilitar la carga de entidades externas (ya hecho)
-    libxml_disable_entity_loader(true);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($strings['title'], ENT_QUOTES, 'UTF-8') ?></title>
+    <title><?= $strings['title'] ?></title>
     <link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
@@ -21,14 +13,14 @@
     <div class="row">
         <div class="col-md-3"></div>   
         <div class="col-md-6">
-            <h1 class="mt-4 text-grey"><?= htmlspecialchars($strings['title'], ENT_QUOTES, 'UTF-8') ?></h1>
-            <p class="mt-4"><button class="w-75 btn btn-primary" type="button" onclick="xmlLoad()"><?= htmlspecialchars($strings['button'], ENT_QUOTES, 'UTF-8') ?></button></p>
+            <h1 class="mt-4 text-grey"><?= $strings['title'] ?></h1>
+            <p class="mt-4"><button class="w-75 btn btn-primary" type="button" onclick="xmlLoad()"><?= $strings['button'] ?></button></p>
         </div>
         <div class="col-md-3"></div> 
     </div>
 </div>
 
-<script id="VLBar" title="<?= htmlspecialchars($strings['title'], ENT_QUOTES, 'UTF-8') ?>" category-id="5" src="/public/assets/js/vlnav.min.js"></script>
+<script id="VLBar" title="<?= $strings['title']; ?>" category-id="5" src="/public/assets/js/vlnav.min.js"></script>
 
 <script type="text/javascript">
 
@@ -37,6 +29,12 @@ function xmlLoad(){
  
   xhttp.open("POST", "test.php", true);
   xhttp.setRequestHeader("Content-type", "text/xml; charset=UTF-8");
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      // No se realiza la redirección aquí
+    }
+  };
   xhttp.send('<city><![CDATA[<title>Karabuk</title><amount>293</amount>]]></city>');
 }
 
