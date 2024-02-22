a714d29ca4b4edfbf122fbb192f05450e30d05d7
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-22 18:59:42 +0100

    Update user.php

--- VulnLab/app/lab/insecure-deserialization/admin-account-1/user.php
+++ VulnLab/app/lab/insecure-deserialization/admin-account-1/user.php
@@ -1,22 +1,27 @@
 <?php
 
-    class User{
+class User {
+    public $username;
+    public $password;
 
-        public $username;
-        public $password;
- 
-
-        function __construct($username,$password) {
-            $this->username = $username;
-            $this->password = $password;
-          }
-
-        function __toString(){
-
-            return "Name : ".$this -> username." Surname : ".$this -> password;
-        }
-
+    function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
     }
 
+    function __toString() {
+        return "Name : " . $this->username . " Surname : " . $this->password;
+    }
+
+    // Evita la serialización de la instancia de usuario
+    public function __sleep() {
+        return [];
+    }
+
+    // Evita la deserialización de la instancia de usuario
+    public function __wakeup() {
+        // Se puede agregar alguna lógica adicional aquí si es necesario
+    }
+}
 
 ?>
