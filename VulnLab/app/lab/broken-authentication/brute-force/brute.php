18eb20e5374a679a42dd6ae8e6da10e871c038d5
oriolnovillo <oriol.novillo@insbaixcamp.cat>
2024-02-22 19:22:15 +0100

    he posat limit de intents del login

--- VulnLab/app/lab/broken-authentication/brute-force/brute.php
+++ VulnLab/app/lab/broken-authentication/brute-force/brute.php
@@ -3,7 +3,6 @@ $db = new PDO('sqlite:users.db');
 $html = "";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-    // Utiliza declaracions preparades per evitar injecció SQL
     $q = $db->prepare("SELECT * FROM users_ WHERE username=:user AND password=:pass");
     $q->execute(array(
         'user' => $_POST['username'],
@@ -12,7 +11,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $_select = $q->fetch();
 
     if (isset($_select['id'])) {
-        // Inicia sessió y estableix variables de sessió
         session_start();
         $_SESSION['username'] = $_POST['username'];
         $html = $strings["cong"];
@@ -21,3 +19,4 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     }
 }
 ?>
+
