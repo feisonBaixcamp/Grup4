c058ae8d4502d8bc561ea25ccdce9485bdfcb8c3
HugoMorenoMoreno <91657687+HugoMorenoMoreno@users.noreply.github.com>
2024-02-22 19:17:41 +0100

    Update randomGenerator.php

--- VulnLab/app/lab/insecure-deserialization/random-nick-generator/randomGenerator.php
+++ VulnLab/app/lab/insecure-deserialization/random-nick-generator/randomGenerator.php
@@ -1,3 +1,16 @@
 <?php
-    print $argv[1]."".rand();
+// Verifica si se proporciona un argumento en la línea de comandos
+if(isset($argv[1])) {
+    // Sanitiza el valor del argumento
+    $input = filter_var($argv[1], FILTER_SANITIZE_STRING);
+    
+    // Genera un número aleatorio seguro
+    $randomNumber = random_int(PHP_INT_MIN, PHP_INT_MAX);
+    
+    // Imprime el valor del argumento concatenado con el número aleatorio
+    print $input . $randomNumber;
+} else {
+    // Si no se proporciona ningún argumento, muestra un mensaje de error
+    echo "Por favor, proporciona un argumento.";
+}
 ?>
